04cab28d7d01eac50db29f698df6066b05c8d53b
Bastien Goulet <bastien.goulet27@outlook.com>
2022-02-16 13:37:24 -0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using INF27507_Boutique_En_Ligne.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace INF27507_Boutique_En_Ligne.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
